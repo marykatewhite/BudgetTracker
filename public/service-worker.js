dbd33fcd0d9158df93c31acc7dd69939fa1a7815
MaryEtak <marykatewhite@gmail.com>
2020-03-21 16:28:19 -0400

    manifest.json and tweaks to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,12 +8,12 @@ const FILES_TO_CACHE = [
   "/icons/icon-512x512.png"
 ];
 
-const PRECACHE = "precache-v1";
-const RUNTIME = "runtime";
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", function(evt) {
   evt.waitUntil(
-    caches.open(PRECACHE).then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -21,53 +21,42 @@ self.addEventListener("install", function(evt) {
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener("activate", event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches
-      .keys()
-      .then(cacheNames => {
-        return cacheNames.filter(
-          cacheName => !currentCaches.includes(cacheName)
-        );
-      })
-      .then(cachesToDelete => {
+  event
+    .waitUntil(
+      caches.keys().then(keysList => {
         return Promise.all(
-          cachesToDelete.map(cacheToDelete => {
-            return caches.delete(cacheToDelete);
+          keysList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
           })
         );
       })
-      .then(() => self.clients.claim())
-  );
+    )
+    .then(() => self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.url.startsWith(self.location.origin)) {
+  if (event.request.url.includes("/api")) {
     event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
           });
-        });
       })
     );
   }
 });
 
-// Serve static files from the cache
-// Proceed with a network request when the resource is not in the cache
-// Allows the page to be accessible offline
-evt.respondWith(
+event.respondWith(
   caches.open(CACHE_NAME).then(cache => {
-    return cache.match(evt.request).then(response => {
-      return response || fetch(evt.request);
+    return cache.match(event.request).then(response => {
+      return response || fetch(event.request);
     });
   })
 );
